94192b98a61461abf69645400b1f0f64f660ba21
Selene Grossett <selene_816@hotmail.com>
2021-11-23 02:01:34 +0000

    added missing semicolon

--- head.js
+++ head.js
@@ -1,20 +1,21 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual[0]} === ${expected}`);
   } else {
-    console.log(`✅✅✅ Assertion Failed:S ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed:S ${actual}${[0]} !== ${expected}`);
   }
 };
 
 //FUNCTION WHICH RETURNS THE FIRST ITEM IN THE ARRAY
 const head = (arr) => {
   return arr[0];
-}
+};
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 console.log(head([5,6,7]));
 console.log(head(["Hello", "Lighthouse", "Labs"]));
+
