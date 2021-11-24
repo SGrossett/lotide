e4727215c3026d8b0de853be995eab4e697a6bb3
Selene Grossett <selene_816@hotmail.com>
2021-11-24 00:39:30 +0000

    function which takes in two arrays and returns true or false, based on a perfect match

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } 
+    }
+  } return true;
+}
+
+// TEST CODE
+(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
