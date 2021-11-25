4be2092784b82d991faed48b60693879a447737e
Selene Grossett <selene_816@hotmail.com>
2021-11-25 13:35:48 +0000

    a function which will return a subset of a given array, removing unwanted elements

---
+++ without.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:S ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } return true;
+};
+
+const without = function(arr, remove) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!remove.includes(arr[i])) newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); 
+//assertArraysEqual(words, ["hello", "world", "lighthouse"]);a
