be3167444e67c2c67ea9f43f5c9b3c6ed42cd3f3
Selene Grossett <selene_816@hotmail.com>
2021-11-30 01:53:10 +0000

    corrected typos in program

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : 
-    `🛑🛑🛑 Assertion Failed:S ${actual} !== ${expected}`;
+    `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 };
 
 module.exports = assertEqual;
