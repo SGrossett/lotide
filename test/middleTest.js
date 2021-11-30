f50a9bb1214ecfd0e67687ab10f5ea92bdec4dac
Selene Grossett <selene_816@hotmail.com>
2021-11-30 02:17:10 +0000

    removed console.logs

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,8 +2,8 @@ const eqArrays = require("../eqArrays");
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+middle([1, 2]);
+middle([1, 2, 3, 4, 5]);
+middle([1, 2, 3, 4, 5, 6]);
 
-console.log(assertArraysEqual(eqArrays([1, 2, 3], [2, 3]), true)); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [2, 3]), true); // => should PASS
