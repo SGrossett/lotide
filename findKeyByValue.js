3f5d72246a8da94545ee1310bf0f1c06bbd75698
Selene Grossett <selene_816@hotmail.com>
2021-11-30 06:08:00 +0000

    removed assertions and exported findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,16 +1,7 @@
-const assertEqual = require('./assertEqual');
-
 const findKeyByValue = (obj, value) => {
   if (Object.values(obj).includes(value)) {
     return Object.keys(obj).find(key => obj[key] === value);
   }
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+module.exports = findKeyByValue;
