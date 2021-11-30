938a52070b826537e3325bee9fb7bb19e628dd54
Selene Grossett <selene_816@hotmail.com>
2021-11-30 07:04:40 +0000

    dried up wet code

--- without.js
+++ without.js
@@ -1,22 +1,5 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:S ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const without = function(arr, remove) {
   const newArr = [];
@@ -26,9 +9,3 @@ const without = function(arr, remove) {
   return newArr;
 }
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); 
-//assertArraysEqual(words, ["hello", "world", "lighthouse"]);a
