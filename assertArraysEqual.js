febbde3d0685e12535c27bd58be629e1b7a453cc
Selene Grossett <selene_816@hotmail.com>
2021-11-30 02:03:08 +0000

    dried up wet code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,8 @@
 const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:S ${actual} !== ${expected}`);
-  }
+  return eqArrays(actual, expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
+    `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 };
 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (var i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    }
-  } return true;
-}
+const eqArrays = require('./eqArrays');
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = assertArraysEqual;
