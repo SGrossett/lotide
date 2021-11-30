80ecbc7dafc113fb64e37cd8f36c21fd71dd978d
Selene Grossett <selene_816@hotmail.com>
2021-11-30 02:51:39 +0000

    cleaned up the code and removed unnecessary comments

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = require('./assertEqual');
 
-//FUNCTION WHICH RETURNS THE FIRST ITEM IN THE ARRAY
 const head = (arr) => {
   return arr[0];
 };
 
 module.exports = head;
-
-//console.log(head([5,6,7]));
-//console.log(head(["Hello", "Lighthouse", "Labs"]));
