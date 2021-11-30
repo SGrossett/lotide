14ebd864fb7cfac53e6edad61b21d5d2183a3deb
Selene Grossett <selene_816@hotmail.com>
2021-11-30 07:00:48 +0000

    set up test case for takeUnit.js using Mocha and Chai

---
+++ test/takeUntilTest.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const result1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const result2 = takeUntil(data2, x => x === ',');
+
+describe('#takeUntil', () => {
+  it('it should return an empty array [] for result1', () => {
+    assert.deepEqual(result1, [ 1, 2, 5, 7, 2 ]);
+  });
+
+  it('it should return [ "I\'ve", "been", "to", "Hollywod" ] for result2', () => {
+    assert.deepEqual(result2, [ "I've", 'been', 'to', 'Hollywood' ]);
+  });
+});
