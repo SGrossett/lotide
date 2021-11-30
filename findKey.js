ed6efbef0e0efac44afa063e4f35155bc61734ce
Selene Grossett <selene_816@hotmail.com>
2021-11-30 05:54:07 +0000

    dried up wet code

--- findKey.js
+++ findKey.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : 
-    `🛑🛑🛑 Assertion Failed:S ${actual} !== ${expected}`;
-};
+const assertEqual = require('./assertEqual');
 
 const findKey = (obj, callback) => {
   let objKeysArr = Object.keys(obj);  
@@ -11,23 +8,4 @@ const findKey = (obj, callback) => {
   }
 };
 
-let result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-
-let result2 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 5)
-
-assertEqual(result1, "noma");
-assertEqual(result2, undefined);
+module.exports = findKey;
