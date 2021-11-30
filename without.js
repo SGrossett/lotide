e3bc3b4aea8cd05edd4737ccded5938ead28ecf5
Selene Grossett <selene_816@hotmail.com>
2021-11-30 07:16:40 +0000

    removed assertions and exported without

--- without.js
+++ without.js
@@ -1,11 +1,9 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
-
 const without = function(arr, remove) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (!remove.includes(arr[i])) newArr.push(arr[i]);
   }
   return newArr;
-}
+};
 
+module.exports = without;
