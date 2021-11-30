b5fef6a2593e86e764e70ec581e00e2a0ca39da6
Selene Grossett <selene_816@hotmail.com>
2021-11-30 05:55:27 +0000

    set up test cases for findKey.js using Mocha and Chai

---
+++ test/findKeysTest.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5)
+
+describe('#countLetters', () => {
+  it('it should return "noma" for result1', () => {
+    assert.strictEqual(result1, "noma");
+  });
+
+  it('it should return undefined for result2', () => {
+    assert.strictEqual(result2, undefined);
+  });
+});
