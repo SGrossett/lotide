5485123408b4bca793b6a2657a50641d8b1665b4
Selene Grossett <selene_816@hotmail.com>
2021-11-30 05:45:27 +0000

    removed assertions and cleaned up comments in the code

--- eqObjects.js
+++ eqObjects.js
@@ -1,8 +1,3 @@
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays');
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   let objKeysArr1 = Object.keys(object1);
   let objKeysArr2 = Object.keys(object2);
